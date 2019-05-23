5615beedc0a68d5ccf19809c07b7ac19ae7b62f7
Si <bokega>
2019-05-24 00:44:13 +0900

    タイトルの作成

---
+++ BigMaze/Assets/Script/StartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScript : MonoBehaviour{
+  public Canvas canvas;
+  public Text text;
+    // Start is called before the first frame update
+    void Start(){
+      canvas.enabled = false;
+      text.text = "ready...";
+    }
+
+    // Update is called once per frame
+    void Update(){
+      if (Input.GetKey(KeyCode.Space)){
+        canvas.enabled = true;
+      }
+    }
+    public void OnButtonClick(){//　OnButtonClickメソッド
+      text.text ="ok!!";
+    }
+}
