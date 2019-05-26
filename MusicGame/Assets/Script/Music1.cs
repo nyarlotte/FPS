e72da77e91f5dc744b7e6f4490db6b0a078e1d5d
Si <bokega>
2019-05-26 23:08:10 +0900

    はい　コミット

---
+++ MusicGame/Assets/Script/Music1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music1 : MonoBehaviour
+{
+  public GameObject Red;
+  public GameObject Blue;
+    // Start is called before the first frame update
+    void Start()
+    {
+      Instantiate(Red);
+      Instantiate(Blue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      new Vector3(0,0,1);
+    }
+}
