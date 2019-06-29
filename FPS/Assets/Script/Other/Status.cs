016c8ee9bb8484362c1b712ca9cd7bc011d65edd
Si <bokega>
2019-06-29 22:38:44 +0900

    フィールド上からプレイヤーのHPを取得できるよ卯に変更　変更スクリプトSchool

---
+++ FPS/Assets/Script/Other/Status.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+   public float HP = 100;
+   public float AT = 45;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- FPS/Assets/Script/Scene/School.cs
+++ FPS/Assets/Script/Scene/School.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class School : MonoBehaviour
 {
 
     GameObject Unitychan;
-
+    public  Slider slider;
+    public  Text UIHP;
+    [SerializeField] GameObject[] Player;
+          Status Script ;
     void Awake (){
       Unitychan=(GameObject)Resources.Load("Kohaku");
-
       Setpos();
-    }
+      slider = GameObject.Find("Slider").GetComponent<Slider>();
+      Player = GameObject.FindGameObjectsWithTag("Player");
+      Script = Player[0].GetComponent<Status>();
+
+      }
     void Start (){
 
     }
    void Update(){
-
+     HPbar();
     }
 
     void Setpos(){
@@ -28,6 +34,10 @@ public class School : MonoBehaviour
         //Instantiate(Misaki,new Vector3(0,0,0),Quaternion.identity);
               break;
       }
-
+    }
+    void HPbar(){
+        slider.maxValue = Script.HP;
+        UIHP.text = "HP"+ Script.HP.ToString() + "/" + Script.HP.ToString();
+        slider.value = Script.HP;
     }
 }
