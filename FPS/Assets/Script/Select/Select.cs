a3d7291d73d1678c5b746962d6a27755e469fab7
Si <bokega>
2019-07-30 20:55:57 +0900

    Select画面　3人を選択できるようにした

---
+++ FPS/Assets/Script/Select/Select.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Select : MonoBehaviour {
+
+  [SerializeField] GameObject _Select, _Panel;
+  [SerializeField] Text _Txt;
+
+
+  public void KohakuClick() {
+    _Select.SetActive(true);
+    _Panel.SetActive(true);
+    _Txt.text = "Unitychan-dayo";
+    PlayerPrefs.SetInt("Character", 0);
+    }
+
+  public void YukoClick() {
+    _Select.SetActive(true);
+    _Panel.SetActive(true);
+    _Txt.text = "Yuko-dayo";
+    PlayerPrefs.SetInt("Character", 1);
+    }
+
+  public void MisakiClick() {
+    _Select.SetActive(true);
+    _Panel.SetActive(true);
+    _Txt.text = "Misaki-dayo";
+    PlayerPrefs.SetInt("Character", 2);
+    }
+}
--- FPS/Assets/Script/Select/SelectUnitychan.cs
+++ FPS/Assets/Script/Select/SelectUnitychan.cs
@@ -9,17 +9,17 @@ public class SelectUnitychan : MonoBehaviour
   public GameObject Select;
   public GameObject panel;
   public GameObject UnitychanText;
-          Text textttt;
+          Text text;
   public bool   Unitychan = false;
 
 public void Start (){
-  textttt =UnitychanText.GetComponent<Text>();
+  text =UnitychanText.GetComponent<Text>();
 }
 
 public void SelectCharacter(){
   Select.SetActive(true);
   panel.SetActive(true);
-  textttt.text = "unitychan-dayo";
+  text.text = "unitychan-dayo";
   Debug.Log(Unitychan);
   PlayerPrefs.SetInt("Character",0);
 }
